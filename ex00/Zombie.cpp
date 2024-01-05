cb1bb36e9ff8cab065544b3af920ce3e6afb0420
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-05 18:47:24 +0000

    ex00: Created zombie class and its member function 'announce'

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 18:42:01 by mortins-          #+#    #+#             */
+/*   Updated: 2024/01/05 18:45:46 by mortins-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(void) {
+}
+
+Zombie::~Zombie(void) {
+	std::cout << this->name << ": Desctructor was called" <<std::endl;
+}
+
+void	Zombie::announce(void) {
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
