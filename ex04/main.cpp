e54058001eba777495c62251877d3e72a9d5d971
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-11 17:46:39 +0000

    ex04: Some progress, it's not working though

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:08:14 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/11 16:43:24 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/01/11 17:44:57 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,38 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 
+	std::ifstream oldFile(argv[1]);
 	std::string filename = argv[1];
-	std::fstream oldFile(filename);
-	std::fstream newFile(filename.append(".replace"));
-	std::string line = "";
+	filename += ".replace";
+	std::ofstream newFile(filename.c_str());
+
+	std::string oldString = argv[2];
+	std::string newString = argv[3];
+	std::string newLine;
+	std::string line;
+	int	subPos = 0;
+	int	pos = 0;
 
 	while (getline(oldFile, line))
 	{
-		if (line.compare(argv[2]) == 0)
-			newFile << argv[3];
+		subPos = line.find(oldString);
+		pos = 0;
+		if (subPos < 0)
+			newFile << line << std::endl;
 		else
-			newFile << line;
+		{
+			newLine.clear();
+			while (subPos >= 0) //this while is working incorrectly
+			{
+				newLine += line.substr(pos, subPos) + newString;
+				subPos = line.find(oldString, newLine.length());
+				if (subPos >= 0)
+					pos = subPos + oldString.length();
+			}
+			newLine += line.substr(pos, -1);
+			newFile << newLine << std::endl;
+		}
+		subPos = 0;
 	}
 	newFile.close();
 	oldFile.close();
