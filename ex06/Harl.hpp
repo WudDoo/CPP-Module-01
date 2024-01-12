f3d92e57aeea0188dec4d95e0fa22f6fe0b635dd
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-12 16:02:16 +0000

    ex06: Added necessary files

---
+++ ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 14:42:07 by mortins-          #+#    #+#             */
+/*   Updated: 2024/01/12 14:46:09 by mortins-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl {
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+
+	public:
+		Harl();
+		~Harl();
+		void	complain( std::string level );
+};
+
+#endif
