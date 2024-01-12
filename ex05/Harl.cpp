07b18b3285be2971bbbb7f89eb6aa4b693d9cc20
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-12 16:00:46 +0000

    ex05: Changed the complain messages

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 14:46:17 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/12 15:47:05 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/01/12 15:59:53 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ Harl::~Harl( void ) {
 }
 
 void	Harl::debug( void ) {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-";
 	std::cout << "pickle-special-ketchup burger. I really do!";
 	std::cout << std::endl;
 }
 
 void	Harl::info( void ) {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money.";
 	std::cout<< "You didn’t put enough bacon in my burger!";
 	std::cout << "If you did, I wouldn’t be asking for more!";
@@ -32,6 +34,7 @@ void	Harl::info( void ) {
 }
 
 void	Harl::warning( void ) {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free.";
 	std::cout << "I’ve been coming for years whereas you started working here ";
 	std::cout << "since last month.";
@@ -39,6 +42,7 @@ void	Harl::warning( void ) {
 }
 
 void	Harl::error( void ) {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now.";
 	std::cout << std::endl;
 }
