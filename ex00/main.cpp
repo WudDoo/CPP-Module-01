422c8bf0c0c6fc9ecc70368791ea8e745c702279
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-17 17:52:35 +0100

    ex00: Improved main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,70 +6,26 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:20:23 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/16 17:14:48 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:49:44 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include "utils.hpp"
 
 int	main(void) {
 	std::string name;
 
-	std::cout << "Creating a zombie in the stack." << std::endl;
-	std::cout << "Zombie name: " << std::flush;
-	std::cin >> name;
-
-	Zombie zombie1(name);
+	std::cout << "Creating a zombie in the stack." << std::endl << std::flush;
+	Zombie zombie1(getInput("* Zombie name: "));
 	zombie1.announce();
 
-	std::cout << "Creating a zombie in the heap using newZombie." << std::endl;
-	std::cout << "Zombie name: " << std::flush;
-	std::cin >> name;
+	std::cout << "Creating a zombie in the stack using randomChump." << std::endl << std::flush;
+	randomChump(getInput("* Zombie name: "));
 
-	Zombie *zombie2 = newZombie(name);
+	
+	std::cout << "Creating a zombie in the heap using newZombie." << std::endl << std::flush;
+	Zombie *zombie2 = newZombie(getInput("* Zombie name: "));
 	zombie2->announce();
 	delete(zombie2);
-
-	std::cout << "Creating a zombie in the stack using randomChump." << std::endl;
-	std::cout << "Zombie name: " << std::flush;
-	std::cin >> name;
-
-	randomChump(name);
 }
-
-// Should properly interpret Ctrl+D
-
-// To allow names with multiple words I need to create utils.cpp and utils.hpp and add the following code block to them.
-// Must also replace `std::cin >> name;` with `name=getInput(<prompt>);`
-/* std::string getInput(const std::string prompt) {
-	std::string input = "";
-
-	while (true)
-	{
-		std::cout << prompt;
-		std::getline(std::cin, input);
-		if (std::cin.eof())
-		{
-			std::cout << std::endl;
-			std::exit(0);
-		}
-		if (std::cin.good() && !input.empty() && !isAllWhitespaces(input))
-			break ;
-		else
-		{
-			std::cin.clear();
-			std::cout << "\033[91mInvalid input, try again.\033[39m" << std::endl;
-		}
-	}
-	return (input);
-}
-
-bool	isAllWhitespaces(const std::string str)
-{
-	for (size_t i = 0; str[i]; i++)
-	{
-		if (!std::isspace(str[i]))
-			return (false);
-	}
-	return (true);
-} */
--- ex00/utils.hpp
+++ ex00/utils.hpp
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/05 18:39:16 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/17 14:42:04 by mortins-         ###   ########.fr       */
+/*   Created: 2024/05/17 17:31:04 by mortins-          #+#    #+#             */
+/*   Updated: 2024/05/17 17:39:46 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-# define ZOMBIE_HPP
+#ifndef UTILS_HPP
+# define UTILS_HPP
 
 # include <iostream>
 # include <cstdlib>
 
-class Zombie {
-	private:
-		std::string	name;
+std::string getInput(const std::string prompt);
 
-	public:
-		Zombie( std::string name );
-		~Zombie( void );
-		void	setName( std::string name );
-		void	announce( void );
-};
-
-Zombie*	newZombie( std::string name );
-void	randomChump( std::string name );
+bool	isAllWhitespaces(const std::string str);
 
 #endif
