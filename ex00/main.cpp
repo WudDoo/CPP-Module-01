e7c2a74da7be7f59e6a0fac597c00bbdb9598e0d
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-17 17:59:11 +0100

    ex00: Removed utils.cpp/.hpp
    Unnecessary overcomplication

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,26 +6,25 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:20:23 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/17 17:49:44 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:58:47 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-#include "utils.hpp"
 
 int	main(void) {
 	std::string name;
 
 	std::cout << "Creating a zombie in the stack." << std::endl << std::flush;
-	Zombie zombie1(getInput("* Zombie name: "));
+	Zombie zombie1("Zombie");
 	zombie1.announce();
 
 	std::cout << "Creating a zombie in the stack using randomChump." << std::endl << std::flush;
-	randomChump(getInput("* Zombie name: "));
+	randomChump("Chump");
 
 	
 	std::cout << "Creating a zombie in the heap using newZombie." << std::endl << std::flush;
-	Zombie *zombie2 = newZombie(getInput("* Zombie name: "));
+	Zombie *zombie2 = newZombie("Heap");
 	zombie2->announce();
 	delete(zombie2);
 }
