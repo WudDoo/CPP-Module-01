02ad4987e7293ad31074aa284de9707747685408
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-20 14:27:03 +0100

    ex00: Cosmetic changes

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,24 +6,24 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 18:42:01 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/17 18:25:11 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:26:01 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie::Zombie( void ) : name("Nameless") {
-	//std::cout << "Zombie " << this->name << " has been constructed" <<std::endl;
+	//std::cout << this->name << " has been constructed" <<std::endl;
 }
 
 Zombie::Zombie( std::string name ) : name(name) {
-	//std::cout << "Zombie " << this->name << " has been constructed" <<std::endl;
+	//std::cout << this->name << " has been constructed" <<std::endl;
 }
 
 Zombie::~Zombie( void ) {
-	std::cout << "Zombie " << this->name << " has been \033[0;31mdestroyed\033[0m." <<std::endl;
+	std::cout << this->name << " has been \033[0;31mdestroyed\033[0m." <<std::endl;
 }
 
 void	Zombie::announce( void ) {
-	std::cout << "\033[0;90m\033[47m" << this->name << "\033[0m: BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:20:23 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/17 18:11:14 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:26:26 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,24 @@
 int	main(void) {
 	std::string name;
 
-	std::cout << "Creating a nameless zombie in the stack." << std::endl << std::flush;
+	std::cout << "\e[30m\e[47m>>\e[0m Creating a nameless zombie in the stack." << std::endl << std::flush;
 	Zombie zombie;
 	zombie.announce();
+	std::cout << std::endl << std::flush;
 
-	std::cout << "Creating a zombie in the stack." << std::endl << std::flush;
+	std::cout << "\e[30m\e[47m>>\e[0m Creating a zombie in the stack." << std::endl << std::flush;
 	Zombie zombie1("Zombie");
 	zombie1.announce();
+	std::cout << std::endl << std::flush;
 
-	std::cout << "Creating a zombie in the stack using randomChump." << std::endl << std::flush;
+	std::cout << "\e[30m\e[47m>>\e[0m Creating a zombie in the stack using randomChump." << std::endl << std::flush;
 	randomChump("Chump");
+	std::cout << std::endl << std::flush;
 
-	
-	std::cout << "Creating a zombie in the heap using newZombie." << std::endl << std::flush;
+	std::cout << "\e[30m\e[47m>>\e[0m Creating a zombie in the heap using newZombie." << std::endl << std::flush;
 	Zombie *zombie2 = newZombie("Heap");
 	zombie2->announce();
+	std::cout << std::endl << std::flush;
+
 	delete(zombie2);
 }
