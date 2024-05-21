c42839fae88decca519d07d6c6b18ce54ef8a79e
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-21 16:03:26 +0100

    ex03: Fixed attack before having a weapon on HumanB

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:34:11 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/21 13:19:50 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/21 15:55:09 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB( std::string _name) : name(_name){
+HumanB::HumanB( std::string _name) : name(_name), weapon(NULL){
 	//std::cout << "HumanB " << this->name << " \e[92mconstructed\e[0m" << std::endl;
 }
 
@@ -25,5 +25,8 @@ void	HumanB::setWeapon( Weapon& _weapon ) {
 }
 
 void	HumanB::attack( void ) {
-	std::cout << this->name << " attacks with their " << weapon->getType() << std::endl;
+	if (!weapon)
+		std::cout << this->name << " is unarmed." << std::endl;
+	else
+		std::cout << this->name << " attacks with their " << weapon->getType() << std::endl;
 }
