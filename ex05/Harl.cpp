aa434f34fca8d56463babca47d501d10f9c800ca
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-21 11:39:52 +0100

    ex05: Fixed text formatting; Added comments to Harl.hpp

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 14:46:17 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/21 11:34:53 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/21 11:37:25 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,15 @@ void	Harl::debug( void ) {
 void	Harl::info( void ) {
 	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money.";
-	std::cout<< "You didn't put enough bacon in my burger!";
-	std::cout << "If you did, I wouldn't be asking for more!";
+	std::cout<< " You didn't put enough bacon in my burger!";
+	std::cout << " If you did, I wouldn't be asking for more!";
 	std::cout << std::endl;
 }
 
 void	Harl::warning( void ) {
 	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free.";
-	std::cout << "I've been coming for years whereas you started working here ";
+	std::cout << " I've been coming for years whereas you started working here ";
 	std::cout << "since last month.";
 	std::cout << std::endl;
 }
