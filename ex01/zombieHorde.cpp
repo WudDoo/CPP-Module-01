9d031cd16fca8be93e24604a75f1c17f8201e2d5
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-21 15:47:56 +0100

    ex01: Added protection for case N <= 0

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,17 +6,20 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:22:41 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/17 18:37:04 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/21 15:46:35 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie*	zombieHorde( int N, std::string name ){
+	if ( N <= 0 )
+		return (NULL);
+
 	Zombie* horde = new Zombie[N];
-	
+
 	for (int i = 0; i < N; i++)
 		horde[i].setName(name);
-	
+
 	return (horde);
 }
