641132e266195145c8fddb4be6b7754be36e6f07
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-23 12:12:16 +0100

    ex00: Renamed variables

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,24 +6,24 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 18:42:01 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/21 15:43:19 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:09:43 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie( void ) : _name("Nameless") {
-	//std::cout << this->_name << " has been \e[92mconstructed\e[0m" <<std::endl;
+Zombie::Zombie( void ) : name("Nameless") {
+	//std::cout << this->name << " has been \e[92mconstructed\e[0m" <<std::endl;
 }
 
-Zombie::Zombie( std::string name ) : _name(name) {
-	//std::cout << this->_name << " has been \e[92mconstructed\e[0m" <<std::endl;
+Zombie::Zombie( std::string _name ) : name(_name) {
+	//std::cout << this->name << " has been \e[92mconstructed\e[0m" <<std::endl;
 }
 
 Zombie::~Zombie( void ) {
-	std::cout << this->_name << " has been \033[0;31mdestroyed\033[0m." <<std::endl;
+	std::cout << this->name << " has been \033[0;31mdestroyed\033[0m." <<std::endl;
 }
 
 void	Zombie::announce( void ) {
-	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 18:39:16 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/21 15:41:34 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:09:20 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@
 
 class Zombie {
 	private:
-		std::string	_name;
+		std::string	name;
 
 	public:
 		// Constructors
 		Zombie( void );
-		Zombie( std::string name );
+		Zombie( std::string _name );
 
 		// Destructor
 		~Zombie( void );
